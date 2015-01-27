b036355679b227cd7c9466ad3ef9877447346b62
john layton <johnstewartlayton@gmail.com>
2015-01-27 15:18:04 +1100

    move the source to common folder

--- gulpfile.js
+++ gulpfile.js
@@ -10,19 +10,17 @@ gulp.task( 'default', function () {
   tar = spawn( 'tar', [
     '-cpvzf',
     'dist/jsenv-' + version + '.tar.gz',
-    'bin',
-    'libexec',
-    'completions'
+    'jsenv'
   ] );
   tar.on( 'close', function ( code ) {
     var version = require( './package.json' ).version;
     var filename = './dist/jsenv-' + version + '.tar.gz';
     var contents = require( 'fs' ).readFileSync( filename );
-    gulp.src( './tmpl/jsenv.rb' )
+    gulp.src( './tmpl/buildr.rb' )
       .pipe( tmpl( {
-        version : version,
-        shasum  : shasum( contents )
-      } ) )
+                     version : version,
+                     shasum  : shasum( contents )
+                   } ) )
       .pipe( gulp.dest( '.' ) );
   } );
 } );
